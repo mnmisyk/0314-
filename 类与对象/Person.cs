06d3415d9d48c3767d48f61f1096f2e4e99b234b
kai yang <yangkai0565@126.com>
2020-05-01 16:22:50 +0800

    添加项目文件。

---
+++ 类与对象/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 类与对象
+{
+  public  class Person
+    {
+        private string _name;
+        public string name
+        {
+            //输出属性的时候，会执行get方法
+            get {
+                if(_name!="abc"&& _name !="cba")
+                    {
+                    return "CNM";
+                    }
+
+                return _name;
+            }
+            //给属性赋值，首先执行set
+            set { _name = value; }
+        }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+
+            set {
+                  if(value<0 || value>100)
+                  {
+                      value = 0;
+                   }
+                _age = value;
+                 }
+        }
+        public void CHLSS()
+        {
+            Console.WriteLine("my name is {0},and my age is {1}",this.name,this.Age);
+        }
+    }
+}
---
+++ 类与对象/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 类与对象
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person SunQuan = new Person();
+            SunQuan.name = "SunQuan";
+            SunQuan.Age = -23;
+            SunQuan.CHLSS();
+            Console.WriteLine("ppp{0}",SunQuan.Age);
+            Console.ReadLine();
+        }
+    }
+}
